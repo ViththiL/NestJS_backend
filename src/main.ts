cd43aa5cd85b226a1ec79a1ee09ebf5fe7d2bb6e
ViththiL <viththisha68@gmail.com>
2024-01-08 10:06:15 +0530

    Nest JS changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { exec } from 'child_process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log(`Application is running on: http://localhost:3000`);
+    // Automatically open the default browser after the server starts
+    exec(`start http://localhost:3000`);
+  });
 }
+
 bootstrap();
